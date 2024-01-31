d2f44c60dab201c3f0a17a7dff303dbfac6ed66c
Diego Pacheco <diego@klarway.com>
2024-01-31 17:42:30 -0300

    #fix astro config change to static dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  output: 'server',
-  adapter: vercel({
-    analytics: true,
-  }),
+  output: 'static'
 });
